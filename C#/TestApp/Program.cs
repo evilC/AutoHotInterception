d23c4e778002f7bb7b992753e1981098b47ad189
evilC <evilc@evilc.com>
2018-03-25 16:18:01 +0100

    update test app

--- C#/TestApp/Program.cs
+++ C#/TestApp/Program.cs
@@ -19,14 +19,13 @@ class TestApp
         var str = iw.GetDeviceList();
         // 0x2D = X key
         //iw.SubscribeKey(0x2D, true, new Action<int>((value) =>
-        /*
-        iw.SubscribeKey(2, true, new Action<int>((value) =>
-        {
-            Console.WriteLine("Subscription Value: " + value);
-        //}), 0x413C, 0x2107);
-        }), 0x04F2, 0x0112);
-        //}), 0x046D, 0xC531);
-        */
+
+        //iw.SubscribeKey(2, true, new Action<int>((value) =>
+        //{
+        //    Console.WriteLine("Subscription Value: " + value);
+        ////}), 0x413C, 0x2107);
+        //}), 0x04F2, 0x0112);
+        ////}), 0x046D, 0xC531);
 
         //iw.SubscribeMouseButton(1, true, new Action<int>((value) =>
         //{
@@ -34,16 +33,17 @@ class TestApp
         //    //}), 0x413C, 0x2107);
         //}), 0x46D, 0xC52B);
 
-        iw.SubscribeMouseMovement(false, new Action<int, int>((x, y) =>
-        {
-            Console.WriteLine($"Mouse Axis Value: x={x}, y={y}");
-        //}), 0x46D, 0xC531);   // G700s
-        }), 0x46D, 0xC00C);     // Logitech Wheel Mouse (Blue)
-
-        //iw.SetContextCallback(0x04F2, 0x0112, new Action<int>((value) =>
+        //iw.SubscribeMouseMovement(false, new Action<int, int>((x, y) =>
         //{
-        //    Console.WriteLine("Context Value: " + value);
-        //}));
+        //    Console.WriteLine($"Mouse Axis Value: x={x}, y={y}");
+        ////}), 0x46D, 0xC531);   // G700s
+        //}), 0x46D, 0xC00C);     // Logitech Wheel Mouse (Blue)
+
+        iw.SetContextCallback(0x46D, 0xC00C, new Action<int>((value) =>
+        //iw.SetContextCallback(0x04F2, 0x0112, new Action<int>((value) =>
+        {
+            Console.WriteLine("Context Value: " + value);
+        }));
         while (true)
         {
             Thread.Sleep(100);
