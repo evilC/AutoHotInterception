f9fcf425b4f787d471688dafb287d72265f4ba9b
Clive Galway <clive.galway@ecs-digital.com>
2022-01-13 21:41:47 +0000

    Update Program.cs

--- C#/TestApp/Program.cs
+++ C#/TestApp/Program.cs
@@ -7,12 +7,11 @@ namespace TestApp
     {
         private static void Main()
         {
-            var mt = new MouseTester();
+            //var mt = new MouseTester();
             //var mbt = new MouseButtonsTester();
             //var kt = new KeyboardTester();
             //var kkt = new KeyboardKeyTester();
             //var tt = new TabletTester();
-
             //var sct = new ScanCodeTester();
             var sst = new SetStateTester(TestDevices.WyseKeyboard, AhkKeys.Obj("1"));
             Console.ReadLine();
