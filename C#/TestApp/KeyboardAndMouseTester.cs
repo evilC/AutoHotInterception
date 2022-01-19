52cfc72112b1fa733a264b6f186fbd474bb62b41
Clive Galway <clive.galway@ecs-digital.com>
2022-01-19 18:25:35 +0000

    Add KeyboardAndMouseTester

---
+++ C#/TestApp/KeyboardAndMouseTester.cs
@@ -0,0 +1,47 @@
+﻿using AutoHotInterception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Helpers;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Allows testing of Keyboard all keys and mouse movement at the same time
+    /// </summary>
+    public class KeyboardAndMouseTester
+    {
+        private Manager im = new Manager();
+
+        public KeyboardAndMouseTester(TestDevice testDevice, bool block = false)
+        {
+            AddDevice(testDevice, block);
+        }
+
+        public KeyboardAndMouseTester AddDevice(TestDevice testDevice, bool block = false)
+        {
+            var devId = testDevice.GetDeviceId();
+
+            if (devId == 0) return this;
+            if (devId < 11)
+            {
+                im.SubscribeKeyboard(devId, block, new Action<ushort, int>((code, value) =>
+                {
+                    var keyObj = AhkKeys.Obj(code);
+
+                    Console.WriteLine($"Name: {keyObj.Name}, Code: {keyObj.LogCode()}, State: {value}");
+                }));
+            }
+            else
+            {
+                im.SubscribeMouseMove(devId, block, new Action<int, int>((x, y) =>
+                {
+                    Console.WriteLine($"Mouse Move: x: {x}, y: {y}");
+                }));
+            }
+            return this;
+        }
+    }
+}
--- C#/TestApp/Program.cs
+++ C#/TestApp/Program.cs
@@ -11,9 +11,10 @@ namespace TestApp
             //var mbt = new MouseButtonTester(TestDevices.LogitechWheelMouse, MouseButtons.Left, true);
             //var ambt = new MouseButtonsTester(TestDevices.LogitechWheelMouse, true);
             //var kt = new KeyboardTester(TestDevices.WyseKeyboard, true);
+            var kmt = new KeyboardAndMouseTester(TestDevices.WyseKeyboard, true).AddDevice(TestDevices.LogitechWheelMouse, true);
             //var kkt = new KeyboardKeyTester(TestDevices.WyseKeyboard, AhkKeys.Obj("1"), true);
             //var tt = new TabletTester(TestDevices.ParbloIslandA609);
-            var sct = new ScanCodeTester(TestDevices.WyseKeyboard, true);
+            //var sct = new ScanCodeTester(TestDevices.WyseKeyboard, true);
             //var sst = new SetStateTester(TestDevices.WyseKeyboard, AhkKeys.Obj("1"));
             Console.ReadLine();
         }
